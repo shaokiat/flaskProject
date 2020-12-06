b693edec4de7624b7b4a68e1a008df714076d5c4
shaokiat <shaokiat@gmail.com>
2020-12-06 18:47:18 +0800

    tutorial part 2: add styling and navigation

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
@@ -10,7 +10,7 @@ posts = [
         'date_posted': 'December 6, 2020'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Victoria',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'December 6, 2020'
@@ -26,7 +26,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
     app.run(debug=True);
